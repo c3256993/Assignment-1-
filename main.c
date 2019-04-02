9a92ef6362888276f11ae893b9502265f223422c
Ben Day <ben.anderson.day@gmail.com>
2019-04-02 02:24:20 +0000

    didnt really get very far

--- main.c
+++ main.c
@@ -2,7 +2,27 @@
 
 int main()
 {
-    printf("Do the goddamn assignment");
+    int i;
+    int k=3;
+    char strOUT[50];
+    char letter;
+    
+    
+    char strIN[] = "TRY TO ENCRYPT THIS";
+    
+    
+    for (i=0; strIN[i] !=0; i++)
+    {
+      letter = strIN[i];  
+    }
+
+    if (letter >= 'a' && letter <= 'b')
+    {
+        
+    }
+    //srtOUT=(strIN+k)%26;
+   
+   printf("%s\n", strOUT);
     
     return 0;
     
