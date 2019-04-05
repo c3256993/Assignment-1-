93a944d80e6f5d67d3a0ab3e73385b277ce978f1
Ben Day <ben.anderson.day@gmail.com>
2019-04-05 02:28:13 +0000

    tried to do file thing

--- main.c
+++ main.c
@@ -1,25 +1,41 @@
 #include<stdio.h>
+#include <string.h>
 
 int main()
+
 {
-	char str[] = "ben"; //hardcoded thing
-	int i=0;
+	//FILE*input;
+	
+	char str[]= "BEN"; //hardcoded thing
+	int i=0;// j=0;
 	int num[i];
 	int k=0;
 	
-	while (i < 3 ){
+	/*double input_status;
+	
+	input = fopen("U:\\ENGG1003\\2019input", "r");
+	if (input == 0){
+	    perror("fopen()");
+	    
+	   }
+	
+	do{
+	    input_status = fscanf(input, "%s", str); //scans the name to the thing
+	    j++;
+	} while (input_status != 0);
+	*/
+	
+	while (num[i] != 0 ){
 	    
 	    num[i]= (int)str[i];
-	    num[i] = (num[i] + k)%26 ;
+	    num[i] = (num[i] + k )  ;
 	    
-	    num[i] = num[i] + 65;
+	   
 	    printf("%c ", num[i]);
 	    
 	    i++;
     }
 	
-	
-
 return 0;
 
 }
