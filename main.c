87ffb7ac6254522a56e04d58274fe0786c7ab641
Ben Day <ben.anderson.day@gmail.com>
2019-04-09 03:08:05 +0000

    file opening in one big main

--- main.c
+++ main.c
@@ -5,22 +5,42 @@ int main()
 
 {
 	
-	char str[]= "BENDAY"; //hardcoded thing
+	FILE*input;
+ 
+	//char str[]= "BENDAY"; //hardcoded thing
 	int i=0;
-	int num[i];
+	int num[100];
 	int k=2;
+	char str[100];
+	 
+ input = fopen("input.txt", "r");
+ 
+ while(1){
+     
+     fscanf(input, "%c", &str[i]); //read character BUT change c to an array like message[i0
+     if (feof(input)){
+         break;
+     }
+     // probally fo call to function here eg. doEncryption(&c)
+     
+     printf("%c", str[i]);
+     i++;
+     
+ }
 	
+
 	// on ascii the capital A starts from 65 and goes until 90
 	
-	while (str[i] != '\0' ) {
+	for (int i=0; str[i] != '\0'; i++) {
 	    
-	    num[i]= (int)str[i] ;
+	    num[i]= str[i];
 	    num[i] = (((num[i] - 65 ) + k)%26) + 65 ; //formula to change ascii to 0 then add the key then apply modulus 
-	   
-	    printf("%c ", num[i]);           //type cast the ascii numbers into characters 
 	    
-	    i++;
+	    printf("\n%c ", num[i]);           //type cast the ascii numbers into characters 
+	      
     }
+
+
+
 return 0;
- 
 }
